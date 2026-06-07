3584fd7a70f85a14719aaf12a2ccfb7da8eb9aeb
Viktor Zepeda <vza12@sfu.ca>
2026-06-07 00:06:04 -0700

    figuring out the structure of items

---
+++ Assets/Scripts/ItemScripts/IGrabbable.cs
@@ -0,0 +1,5 @@
+using UnityEngine;
+
+public interface IGrabbable {
+    public void Grab();
+}
---
+++ Assets/Scripts/ItemScripts/Throwables/IThrowable.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public interface IThrowable {
+    
+    public void Throw();
+}
